75534a1d13806d15a1f0e4d0a993bc90d53dc00b
armandriusfcm <armand.rius@es.fcm.travel>
2025-03-19 20:55:00 +0100

    Implement line items

--- lib/checkout_system/models/line_item.rb
+++ lib/checkout_system/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem
-  # TODO don't let pricing rules be exposed as an array. Review attribute accessors
+  # TODO: don't let pricing rules be exposed as an array. Review attribute accessors
   attr_reader :checkout, :product, :quantity, :final_price, :pricing_rules_applied
 
   def initialize(product:, quantity: 1, checkout: nil)
@@ -8,5 +8,36 @@ class LineItem
     @quantity = quantity
     @final_price = product.price * quantity
     @pricing_rules_applied = []
+  end
+
+  def increment
+    raise "Can't modify quantity because a pricing rule was applied" if pricing_rules_applied.any?
+
+    @quantity += 1
+    refresh_final_price
+  end
+
+  def decrement
+    raise "Can't modify quantity because a pricing rule was applied" if pricing_rules_applied.any?
+
+    @quantity = [0, @quantity - 1].max
+    refresh_final_price
+  end
+
+  def apply_pricing_rule(pricing_rule)
+    unless pricing_rule.is_a?(PricingRules::Base)
+      raise ArgumentError,
+            "Expected a pricing rule, got a #{pricing_rule.class} instead"
+    end
+
+    @final_price = pricing_rule.final_price(self)
+    @pricing_rules_applied << pricing_rule
+  end
+
+  private
+
+  def refresh_final_price
+    @final_price = product.price * quantity
+    quantity
   end
 end
---
+++ spec/models/line_item_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+
+RSpec.describe LineItem do
+  let(:product) { instance_double('Product', price: 100) }
+  let(:line_item) { LineItem.new(product: product, quantity: 2) }
+  let(:pricing_rule) { PricingRules::Base.new }
+
+  shared_examples 'cannot be modified' do
+    subject(:action) { line_item.increment }
+
+    it 'raises an error' do
+      # TODO Better error class
+      expect { action }.to raise_error("Can't modify quantity because a pricing rule was applied")
+    end
+  end
+
+  shared_context 'with a pricing rule applied' do
+    before { line_item.apply_pricing_rule(pricing_rule) }
+  end
+
+  describe '#increment' do
+    context 'when no pricing rule is applied' do
+      before { line_item.increment }
+
+      it 'increases the quantity by 1' do
+        expect(line_item.quantity).to eq(3)
+      end
+
+      it 'updates the final price' do
+        expect(line_item.final_price).to eq(300)
+      end
+    end
+
+    context 'when a pricing rule is applied' do
+      include_context 'with a pricing rule applied'
+      
+      it_behaves_like 'cannot be modified' do
+        subject(:action) { line_item.increment }
+      end
+    end
+  end
+
+  describe '#decrement' do
+    context 'when no pricing rule is applied' do
+      before { line_item.decrement }
+
+      it 'decreases the quantity by 1' do
+        expect(line_item.quantity).to eq(1)
+      end
+
+      it 'updates the final price' do
+        expect(line_item.final_price).to eq(100)
+      end
+
+      it 'does not decrease the quantity below 0' do
+        2.times { line_item.decrement }
+        expect(line_item.quantity).to eq(0)
+      end
+    end
+
+    context 'when a pricing rule is applied' do
+      include_context 'with a pricing rule applied'
+
+      it_behaves_like 'cannot be modified' do
+        subject(:action) { line_item.decrement }
+      end
+    end
+  end
+
+  describe '#apply_pricing_rule' do
+    let(:modified_price) { 150 }
+    before { allow(pricing_rule).to receive(:final_price).and_return(modified_price) }
+
+    it 'applies the pricing rule' do
+      line_item.apply_pricing_rule(pricing_rule)
+      expect(line_item.final_price).to eq(modified_price)
+    end
+
+    it 'adds the pricing rule to the applied rules' do
+      line_item.apply_pricing_rule(pricing_rule)
+      expect(line_item.pricing_rules_applied).to include(pricing_rule)
+    end
+
+    it 'raises an error if the argument is not a pricing rule' do
+      expect { line_item.apply_pricing_rule(instance_double('NotAPricingRule')) }.to raise_error(ArgumentError)
+    end
+  end
+end
